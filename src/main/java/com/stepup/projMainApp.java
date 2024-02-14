828781955feef65b7c3886de3a11fade12036012
Lubov Churkina <git.user.LCHurkina@inno.tech>
2024-02-14 12:24:24 +0300

    Изменена основная логика Добавлена коробка

--- src/main/java/com/stepup/projMainApp.java
+++ src/main/java/com/stepup/projMainApp.java
@@ -2,7 +2,8 @@ package com.stepup;
 
 public class projMainApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
         System.out.println("111111111");
+        System.out.println("222222222");
     }
 }
