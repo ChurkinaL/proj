eaaabd70c10ee0fd0fd32478a18c2f6cdd00ea29
Lubov Churkina <git.user.LCHurkina@inno.tech>
2024-02-14 12:40:51 +0300

    улучшили логику

--- src/main/java/com/stepup/projMainApp.java
+++ src/main/java/com/stepup/projMainApp.java
@@ -4,6 +4,9 @@ public class projMainApp {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("111111111");
-        System.out.println("222222222");
+        System.out.println("--------");
+    }
+    public static void main(String[] args) {
+        System.out.println("000000000");
     }
 }
